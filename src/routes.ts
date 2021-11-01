55f52b9d937fcc0ea0ed1c37ffd5c67c249d114e
bruna-gregorio <blgregorio16@gmail.com>
2021-11-01 14:56:15 -0300

    criando o compliment

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,22 @@
 import { Router } from "express";
 
+import { ensureAdmin } from "./middleware/ensureAdmin";
+
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
-
-import { ensureAdmin } from "./middleware/ensureAdmin";
-
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 const router = Router()
 
 const createUserController = new CreateUserController
 const createTagController = new CreateTagController
 const authenticateUserController = new AuthenticateUserController
+const createComplimentController = new CreateComplimentController
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
+router.post("/compliments", createComplimentController.handle)
 
 export { router }
